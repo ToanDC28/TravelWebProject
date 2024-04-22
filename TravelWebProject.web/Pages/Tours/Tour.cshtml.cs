5081d83c4fc1cc2462862cf0ccb775e39c402848
ToanDC28 <toandcse160630@fpt.edu.vn>
2024-04-22 12:43:06 +0700

    tours

---
+++ TravelWebProject.web/Pages/Tours/Tour.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelWebProject.web.Pages.Tours
+{
+    public class TourModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
