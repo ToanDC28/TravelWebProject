859dea37becd504b452814f304d1f4e67d0e946c
Nguyenluu2805 <luuxuanhoangnguyen@gmail.com>
2024-04-23 10:05:28 +0700

    1

---
+++ TravelWebProject.web/Pages/BookingPage/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Models;
+
+namespace TravelWebProject.web.Pages.BookingPage
+{
+    public class EditModel : PageModel
+    {
+        private readonly BusinessObject.Models.TravelWebContext _context;
+
+        public EditModel(BusinessObject.Models.TravelWebContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Booking Booking { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Bookings == null)
+            {
+                return NotFound();
+            }
+
+            var booking =  await _context.Bookings.FirstOrDefaultAsync(m => m.BookingId == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            Booking = booking;
+           ViewData["TourId"] = new SelectList(_context.Tours, "TourId", "Description");
+           ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Booking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingExists(Booking.BookingId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool BookingExists(int id)
+        {
+          return (_context.Bookings?.Any(e => e.BookingId == id)).GetValueOrDefault();
+        }
+    }
+}
