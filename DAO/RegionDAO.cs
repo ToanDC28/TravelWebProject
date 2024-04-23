28edfb70d4ab42d8ebffd2e80d84043e3eacbc41
ToanDC28 <toandcse160630@fpt.edu.vn>
2024-04-23 10:10:35 +0700

    RegionService

---
+++ DAO/RegionDAO.cs
@@ -0,0 +1,43 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class RegionDAO
+    {
+        private static RegionDAO instance = null;
+        private readonly TravelWebContext context = null;
+
+        private RegionDAO()
+        {
+            context = new TravelWebContext();
+        }
+
+        public static RegionDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new RegionDAO();
+                }
+                return instance;
+            }
+        }
+
+        public List<Region> GetAll()
+        {
+            try
+            {
+                return context.Regions.ToList();
+            }catch (Exception ex)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
