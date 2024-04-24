056c38b49eb1db64743f3fe3d2c443ff4e38e91b
Nguyenluu2805 <luuxuanhoangnguyen@gmail.com>
2024-04-24 08:32:03 +0700

    4

--- TravelWebProject.web/Pages/DestinationPage/Index.cshtml.cs
+++ TravelWebProject.web/Pages/DestinationPage/Index.cshtml.cs
@@ -1,23 +1,23 @@
+using BusinessObject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TravelWebProject.service.DestinationServices;
 
 namespace TravelWebProject.web.Pages.DestinationPage
 {
     public class IndexModel : PageModel
     {
-<<<<<<< Updated upstream
-=======
 
         private readonly IDestinationService destinationService;
-  
+
         public IndexModel()
         {
             this.destinationService = new DestinationService();
         }
         public List<Destination> Destinations { get; set; }
->>>>>>> Stashed changes
         public void OnGet()
         {
+            Destinations = destinationService.GetDestinations();
         }
     }
 }
