241d6d3d32dd3b667c7cf53666418df3e66f31aa
ToanDC28 <toandcse160630@fpt.edu.vn>
2024-04-25 09:13:25 +0700

    booking form edit

--- TravelWebProject.web/Pages/BookingPage/bookingForm.cshtml.cs
+++ TravelWebProject.web/Pages/BookingPage/bookingForm.cshtml.cs
@@ -5,6 +5,7 @@ using TravelWebProject.repo.Users;
 using TravelWebProject.service.BookingService;
 using TravelWebProject.service.Users;
 using System.Security.Claims;
+using TravelWebProject.service.TourServices;
 
 namespace TravelWebProject.web.Pages.BookingPage
 {
@@ -12,17 +13,22 @@ namespace TravelWebProject.web.Pages.BookingPage
     {
         private readonly IBookingService bookingService;
         private readonly IUserService userService;
+        private readonly ITourService tourService;
 
         public bookingFormModel(IBookingService bookingService, IUserService userService, Booking booking)
         {
             this.bookingService = bookingService;
             this.userService = userService;
+            tourService = new TourService();
         }
         [BindProperty]
         public Booking Booking { get; set; }
+        public Tour Tour { get; set; }
         public IActionResult OnGet(int ? id)
         {
-            Booking.TourId = id;
+            Tour = tourService.GetTourById(id.Value);
+            
+            
             var user = HttpContext.User;
             if (user.Identity.IsAuthenticated)
             {
--- TravelWebProject.web/Pages/SignIn.cshtml.cs
+++ TravelWebProject.web/Pages/SignIn.cshtml.cs
@@ -37,7 +37,7 @@ namespace TravelWebProject.web.Pages
             if (User.Identity.IsAuthenticated)
             { 
 
-                return RedirectToPage("/Index");
+                return RedirectToPage("/LandingPage");
             }
 
             return Page();
